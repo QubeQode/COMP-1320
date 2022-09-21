9d072c049d6d5e5b6b114a631292a8e9409abb09
QubeQode <77304768+QubeQode@users.noreply.github.com>
2022-09-21 00:29:35 -0700

    Implement getDayOfTheWeekForUserDate() function

--- Week 2 Lab Assignment/main.js
+++ Week 2 Lab Assignment/main.js
@@ -1,2 +1,23 @@
-const getDayOfTheWeek = require('./lab-two').getDayOfTheWeek;
+const { getDayOfTheWeek } = require('./lab-two');
+const { makeCalendar } = require('./lab-two');
+const readline = require('readline-sync');
 
+makeCalendar();
+
+/*
+ * prints day of the week for the year, month, day inputted by user
+ * readline-sync used for terminal inputs
+ * const year = readline.question('Please provide a year:')
+ * const month = readline.question('Please provide a month:')
+ * const date = readline.question('Please provide a date:')
+ */
+const getDayOfTheWeekForUserDate = (year, month, date) => {
+    const foundDay = getDayOfTheWeek(year, month, date);
+    console.log(foundDay);
+};
+
+const year = readline.question('Please provide a year:');
+const month = readline.question('Please provide a month:');
+const date = readline.question('Please provide a date:');
+
+getDayOfTheWeekForUserDate(year, month, date);
